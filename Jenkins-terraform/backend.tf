b427ee2c87ceb9d9c174cf8bd1656055bff77a36
Rahul  k <gowdarahul1819@gmail.com>
2024-03-29 18:22:18 +0530

    updated s3 region name

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "tetris-4" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
-    region = "ap-sout-1"
+    region = "ap-south-1"
   }
 }
