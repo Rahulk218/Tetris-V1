3c330d3286c4759e38b76b07765a3fcea591fee7
Rahul k <101118956+Rahulk218@users.noreply.github.com>
2024-03-29 21:49:41 +0530

    Updated path for key in backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tetris-4" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
+    key    = "Eks/terraform.tfstate"
     region = "ap-south-1"
   }
 }
